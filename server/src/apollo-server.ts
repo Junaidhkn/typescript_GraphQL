9a1ababd150bc8713ed1ba905eb7760a73a95de7
Junaidhkn <junaidhkniazi@gmail.com>
2023-04-26 02:20:26 +0500

    resolver added

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -3,6 +3,7 @@ import { ApolloServer, ExpressContext } from 'apollo-server-express';
 import * as express from 'express';
 import { Server } from 'http';
 import Db from './db';
+import resolvers from './resolvers';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { addResolversToSchema } from '@graphql-tools/schema';
@@ -17,25 +18,6 @@ export async function createApolloServer(
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  const resolvers = {
-    Query: {
-      currentUser: () => {
-        return {
-          id: '123',
-          name: 'John Doe',
-          handle: 'johndoe',
-          coverUrl: '',
-          avatarUrl: '',
-          createdAt: '',
-          updatedAt: '',
-        };
-      },
-      suggestions: () => {
-        return [];
-      },
-    },
-  };
-
   const server = new ApolloServer({
     schema: addResolversToSchema({
       schema: SCHEMA,
